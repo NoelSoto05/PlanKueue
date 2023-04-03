6e8b0320e5971f4fc5d61fecfe041fd2e66ba41c
Simolic <sotonoel7@yahoo.com>
2023-04-02 19:22:00 -0500

    Initial Commit

---
+++ src/main/java/com/PlanKueue/springbootPlanKueue/SpringBootPlanKueueApplication.java
@@ -0,0 +1,13 @@
+package com.PlanKueue.springbootPlanKueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlanKueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlanKueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PlanKueue/springbootPlanKueue/SpringBootPlanKueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PlanKueue.springbootPlanKueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlanKueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
