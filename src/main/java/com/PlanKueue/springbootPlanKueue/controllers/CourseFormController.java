3bb8de1d1984122322f26a8cd38d905915cdc15b
Noel Soto <93099091+simolic@users.noreply.github.com>
2023-05-01 13:56:02 -0500

    added course button functionality and storage

---
+++ src/main/java/com/PlanKueue/springbootPlanKueue/controllers/CourseFormController.java
@@ -0,0 +1,47 @@
+package com.PlanKueue.springbootPlanKueue.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.PlanKueue.springbootPlanKueue.models.Courses;
+import com.PlanKueue.springbootPlanKueue.repository.CourseRepository;
+
+@Controller
+public class CourseFormController {
+
+    private final Logger logger = LoggerFactory.getLogger(CourseFormController.class);
+
+    @Autowired
+    private CourseRepository courseRepository;
+    @GetMapping("/add-course")
+    public String showCreateForm(Model model) {
+        model.addAttribute("course", new Courses());
+        return "add-course";
+    }
+
+    @GetMapping("/editCourse/{courseId}")
+    public String showUpdateForm(@PathVariable("courseId") long courseId, Model model) {
+        Courses course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course id: " + courseId + " not found"));
+
+        model.addAttribute("course", course);
+        return "update-course";
+
+    }
+
+    @GetMapping("/deletecourse/{courseId}")
+    public String deleteTodoItem(@PathVariable("courseId") long courseId, Model model) {
+        Courses course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Course id: " + courseId + " not found"));
+
+        courseRepository.delete(course);
+        return "redirect:/";
+    }
+
+  
+}
