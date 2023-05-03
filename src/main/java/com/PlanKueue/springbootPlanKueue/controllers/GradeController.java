7611a942bbe556e97dc7da6ec7143ec523cfdd86
mmilan331 <mamilan331@gmail.com>
2023-05-03 14:36:45 -0500

    "Fixed" Calculate GPA button + functionalities

--- src/main/java/com/PlanKueue/springbootPlanKueue/controllers/GradeController.java
+++ src/main/java/com/PlanKueue/springbootPlanKueue/controllers/GradeController.java
@@ -3,25 +3,25 @@ package com.PlanKueue.springbootPlanKueue.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.PlanKueue.springbootPlanKueue.models.Grade;
 
 @Controller
 public class GradeController {
-  
+    @Autowired
     private List<Grade> grades = new ArrayList<Grade>();
-    @RequestMapping("/grades")
-    @GetMapping("/")
+
+    @GetMapping("/GPA")
     public String index(Model model) {
         // add the list of grades to the model
-        model.addAttribute("grades", grades);
-        return "index";
+        model.addAttribute("grade", grades);
+        return "grade";
     }
     
     @PostMapping("/addGrade")
@@ -29,22 +29,26 @@ public class GradeController {
         // add the new grade to the list of grades
         grades.add(grade);
         
-        return "redirect:/";
+        return "grade";
     }
     
-    @GetMapping("/calculateGPA")
+    @PostMapping("/calculateGPA")
     public String calculateGPA(Model model) {
         // calculate the GPA based on the grades in the model
+        double gpa = 0.0;
         double totalPoints = 0.0;
         int totalCredits = 0;
         for (Grade grade : grades) {
             totalPoints += grade.getCredits() * grade.getGrade();
             totalCredits += grade.getCredits();
         }
-        double gpa = totalPoints / totalCredits;
-        
+        gpa = totalPoints / totalCredits;
+
+        for (int i = 0; i < grades.size(); i++){
+            grades.remove(i);
+        }
         // add the GPA to the model and return the GPA view
         model.addAttribute("gpa", gpa);
-        return "gpa";
+        return "grade";
     }
 }
